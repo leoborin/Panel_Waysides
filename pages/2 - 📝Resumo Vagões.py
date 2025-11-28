e2df8b4509a049394639e259d22aad087ece181b
leoborin <leonarborin@gmail.com>
2025-11-28 14:31:32 -0300

    vagão resumo3

--- pages/2 - 📝Resumo Vagões.py
+++ pages/2 - 📝Resumo Vagões.py
@@ -465,8 +465,8 @@ def concatenar_dados_trkv_z851(df_z851, df_trkv):
         .merge(df_last[['key', 'ultima_medicao', 'data_ultima_medicao']],
                on='key', how='left')
 
-    df_final = df_final[["EQUNR", "MODELO", "STATUS", "DATA_DE_FABRICACAO_trated", "DATA_GARANTIA_trated", "ULTIMA_RG",
-                         "KM_RODADO_DESDE_ULTIMA_RG", "max_medicao_ultimas_3", "ultima_medicao", "data_ultima_medicao", "key"]]
+    # df_final = df_final[["EQUNR", "MODELO", "STATUS", "DATA_DE_FABRICACAO_trated", "DATA_GARANTIA_trated", "ULTIMA_RG",
+    #                      "KM_RODADO_DESDE_ULTIMA_RG", "max_medicao_ultimas_3", "ultima_medicao", "data_ultima_medicao", "key"]]
 
     df_tratado = df_final.rename(columns={
         'max_medicao_ultimas_3': 'TRKV_max_medicao_ultimas_3',
@@ -562,7 +562,8 @@ def concatenar_dados_new1_wcm_trated(df_new1, wcm_trated):
         .merge(df_last[['key', 'ultima_medicao', 'data_ultima_medicao']],
                on='key', how='left')
 
-    # df_final = df_final[["EQUNR","MODELO","STATUS","DATA_DE_FABRICACAO_trated","DATA_GARANTIA_trated","ULTIMA_RG","KM_RODADO_DESDE_ULTIMA_RG","max_medicao_ultimas_3","ultima_medicao","data_ultima_medicao","key"]]
+    df_final = df_final[["EQUNR", "MODELO", "STATUS", "DATA_DE_FABRICACAO_trated", "DATA_GARANTIA_trated", "ULTIMA_RG",
+                         "KM_RODADO_DESDE_ULTIMA_RG", "max_medicao_ultimas_3", "ultima_medicao", "data_ultima_medicao", "key"]]
 
     df_tratado = df_final.rename(columns={
         'max_medicao_ultimas_3': 'WCM_max_medicao_ultimas_3',
